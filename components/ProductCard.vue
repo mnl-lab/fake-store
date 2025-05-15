5f8439a6a3204a2312d7becbe06d773bc191e73c
mnl-lab <147846109+mnl-lab@users.noreply.github.com>
2025-05-15 20:49:59 +0100

    cmt

---
+++ components/ProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <img :src="product.image" alt="Product Image" />
+            <h2>{{ product.name }}</h2>
+            <p>{{ product.description }}</p>
+            <p>Price: {{ product.price }}</p>
+            <button class="btn">Add to Cart</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  ssr: true, // or true if you want server-side rendering
+  nitro: {
+    preset: "static"
+  },
+  // runtimeConfig: {
+  //   public: {
+  //     firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+  //     // you can add the rest here
+  //   }
+  // }
 })
